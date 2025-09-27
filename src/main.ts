43ecad13297c96dddf46e5c2da73bb2d0e9a6042
dev.team <dev.team@gradegy.com>
2025-09-27 15:26:24 +0330

    initialze setup for nestjs application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const configService = app.get(ConfigService);
+  const logger = new Logger('Bootstrap');
+
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+  });
+
+  if (configService.get<boolean>('swagger.enabled', true)) {
+    const swaggerDocumentConfig = new DocumentBuilder()
+      .setTitle(configService.get<string>('swagger.title') ?? 'API')
+      .setDescription(configService.get<string>('swagger.description') ?? '')
+      .setVersion(configService.get<string>('swagger.version') ?? '1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerDocumentConfig);
+    const swaggerPath = configService.get<string>('swagger.path') ?? 'docs';
+    SwaggerModule.setup(swaggerPath, app, document, {
+      swaggerOptions: { persistAuthorization: true },
+    });
+  }
+
+  const port = configService.get<number>('PORT') ?? Number(process.env.PORT) ?? 5400;
+  await app.listen(port);
+  logger.log(`Application is running on port ${port}`);
 }
 bootstrap();
